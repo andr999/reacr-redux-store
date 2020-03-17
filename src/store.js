c959f843c2513bb90967963cf72f04069ecef0f6
andr999 <andreykuchay2@gmail.com>
2020-03-17 17:59:42 +0200

    First commit

---
+++ src/store.js
@@ -0,0 +1,31 @@
+import { createStore, applyMiddleware} from 'redux';
+import thunkMiddleware from 'redux-thunk'
+import reducer from './reducers';
+
+const logMiddleware = ({getState}) => (next) => (action) => {
+    console.log(action.type, getState());
+    return next(action);
+};
+
+const stringMiddleware = () => (next) => (action) => {
+    if (typeof action === 'string') {
+        return next({
+            type: action
+        });
+    }
+}
+
+const store = createStore(reducer, applyMiddleware(thunkMiddleware, stringMiddleware, logMiddleware));
+
+store.dispatch('HELLO_WORLD');
+
+
+
+const delayedActionCreator = (timeout) =>(dispatch) => {
+    setTimeout(() => dispatch({
+        type: 'DELAYED_ACTION'
+    }),timeout)
+
+store.dispatch(delayedActionCreator(3000));
+
+export default store;
